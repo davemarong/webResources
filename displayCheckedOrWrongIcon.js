7e2017a211c2536abc0a02ffe9d33faadce4d494
davemarong <kmarong@hotmail.com>
2022-11-07 15:29:35 +0100

    Add comments to code

--- displayCheckedOrWrongIcon.js
+++ displayCheckedOrWrongIcon.js
@@ -1,17 +1,19 @@
 function displayCheckedOrWrongIcon(rowData) {
+  // Get the current row in the loop
   const result = JSON.parse(rowData);
+
+  // Set the image and tooltip variables, which will be returned
   let image;
   let tooltip;
-  console.log(result.fir_deviationstatus);
+
+  // Check if the DeviationStatus on the current row is "Error" or "Correct"
   if (result.fir_deviationstatus === "Error") {
     image = "fir_wrongicon";
     tooltip =
       "There is a deviation between this customer and the one stored in Account";
-    console.log("first", image, tooltip);
   } else if (result.fir_deviationstatus === "Correct") {
     image = "fir_checkicon";
     tooltip = "This customer has the same data as the on stored in Account";
-    console.log("second", image, tooltip);
   }
   return [image, tooltip];
 }
